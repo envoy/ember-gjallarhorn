6d147235d5ad3a621fcfcee76ba9927f40b848dd
Chris Freeman <cfreeman.code@gmail.com>
2018-05-14 13:09:51 -0500

    better nearlyEqual helper

--- tests/helpers/nearly-equal.ts
+++ tests/helpers/nearly-equal.ts
@@ -1,3 +1,6 @@
-export default function nearlyEqual(a: number, b: number, threshold = 5) {
-  return Math.abs(a - b) <= threshold;
+export default function nearlyEqual(a: number, b: number, threshold = 10) {
+  let minDelta = b * ((100 - threshold) / 100);
+  let maxDelta = b * ((100 + threshold) / 100);
+
+  return  minDelta <= a || a <= maxDelta;
 }
