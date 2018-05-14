3f3fb0a827cb1a1dd7ef6b2fdfc25c26db4abe92
Chris Freeman <cfreeman.code@gmail.com>
2018-05-14 13:09:41 -0500

    turn Timer into a Node

--- addon/utils/timer.ts
+++ addon/utils/timer.ts
@@ -1,13 +1,38 @@
-export class Timer {
+interface Node {
+  value: any;
+  children: Array<Node>;
+
+  isRoot: boolean;
+  hasChildren: boolean;
+
+  append(child: Node): Node;
+}
+
+export class Timer implements Node {
+  private _children: Array<Timer> = [];
+  private _parent: Timer | null = null;
+
   startLabel = '';
   stopLabel = '';
 
   constructor(public label: string) {}
 
-  get data() {
+  get value() {
     window.performance.measure(this.label, this.startLabel, this.stopLabel);
     let measures: Array<PerformanceMeasure> = window.performance.getEntriesByName(this.label);
     return measures[0];
+  }
+
+  get isRoot() {
+    return !this._parent;
+  }
+
+  get children() {
+    return this._children;
+  }
+
+  get hasChildren() {
+    return this._children.length > 0;
   }
 
   start() {
@@ -20,10 +45,30 @@ export class Timer {
     window.performance.mark(this.stopLabel);
   }
 
+  appendAndStart(label: string): Timer {
+    let child = new Timer(`${this.label}:${label}`);
+    child._parent = this;
+    this._children.push(child);
+    child.start();
+    return child;
+  }
+
+  append(child: Timer): Timer {
+    child._parent = this;
+    this._children.push(child);
+    return this;
+  }
+
   clear() {
     window.performance.clearMarks(this.startLabel);
     window.performance.clearMarks(this.stopLabel);
     window.performance.clearMeasures(this.label);
+
+    if (this.hasChildren) {
+      this.children.forEach(child => {
+        child.clear();
+      })
+    }
   }
 }
 
--- tests/unit/utils/timer-test.ts
+++ tests/unit/utils/timer-test.ts
@@ -9,7 +9,7 @@ module('Unit | Utility | timer', function(hooks) {
     window.performance.clearMeasures();
   });
 
-  test('Timer works', async function(assert) {
+  test('Timer basically works', async function(assert) {
     let timer = new Timer('testing123');
 
     timer.start();
@@ -18,6 +18,68 @@ module('Unit | Utility | timer', function(hooks) {
 
     timer.stop();
 
-    assert.ok(nearlyEqual(timer.data.duration, 1000));
+    assert.ok(nearlyEqual(timer.value.duration, 1000));
+  });
+
+  test('Timer.append works', async function(assert) {
+    let foo = new Timer('foo');
+    let bar = new Timer('bar');
+    let baz = new Timer('baz');
+
+    foo.start();
+
+    foo.append(bar);
+
+    assert.ok(foo.hasChildren, 'foo should have children after append');
+    assert.equal(foo.children.length, 1, 'foo should have 1 child');
+    assert.ok(foo.children[0] instanceof Timer, "foo's child should be a Timer");
+
+    bar.start();
+    await sleep(50);
+    bar.stop();
+
+
+    bar.append(baz);
+    assert.ok(foo.children[0].hasChildren, "appending to a child should register on the parent");
+    assert.equal(foo.children.length, 1, "appending to a child shouldn't affect the parent's child count");
+
+    baz.start();
+    await sleep(100);
+    baz.stop();
+
+    foo.stop();
+
+    assert.ok(nearlyEqual(foo.value.duration, 150, 10), "foo's duration should be the sum of it's two children");
+    assert.ok(nearlyEqual(bar.value.duration, 50), "bar's duration should be 50ms");
+    assert.ok(nearlyEqual(baz.value.duration, 100), "baz's duration should be 100ms");
+  });
+
+  test('Timer#appendAndStart works', async function(assert) {
+    let foo = new Timer('foo');
+
+    foo.start();
+
+    let bar = foo.appendAndStart('bar');
+
+    assert.ok(foo.hasChildren, 'foo should have children after append');
+    assert.equal(foo.children.length, 1, 'foo should have 1 child');
+    assert.ok(foo.children[0] instanceof Timer, "foo's child should be a Timer");
+
+    await sleep(50);
+    bar.stop();
+
+
+    let baz = bar.appendAndStart('baz');
+    assert.ok(foo.children[0].hasChildren, "appending to a child should register on the parent");
+    assert.equal(foo.children.length, 1, "appending to a child shouldn't affect the parent's child count");
+
+    await sleep(100);
+    baz.stop();
+
+    foo.stop();
+
+    assert.ok(nearlyEqual(foo.value.duration, 150, 10), "foo's duration should be the sum of it's two children");
+    assert.ok(nearlyEqual(bar.value.duration, 50, 10), "bar's duration should be 50ms");
+    assert.ok(nearlyEqual(baz.value.duration, 100, 10), "baz's duration should be 100ms");
   });
 });
